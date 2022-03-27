c206384e770ba821167f74701580ef73d8c18f60
pedropark99 <pedropark99@gmail.com>
2022-03-27 10:34:17 -0300

    Linking to libxml2 working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
 #include "rapidxml/rapidxml.hpp"
-#include "rapidxml/rapidxml_utils.hpp"
-#include "rapidxml/rapidxml_print.hpp"
+
+
+#include "libxml/parser.h"
+
 
 int main (void) {
-    rapidxml::file<> xmlFile("../xml_examples/xmlns1.xml"); 
     rapidxml::xml_document<> doc;
-    doc.parse<0>(xmlFile.data());
-
-    rapidxml::xml_node<> *node = doc.first_node();
-
-    for(rapidxml::xml_node<> *i = doc.first_node(); i; i->next_sibling()){
-        std::cout << i->value() << "\n";
-    }
 
     return 1;
 }
